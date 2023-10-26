b5acd911c7a892f44f1e9e920d6481a308c836d4
Rafael Pereira <rafaelbsides at gmail.com>
2023-10-26 14:12:56 -0300

    💄 feat: Adiciona tipografia ao tailwind (não estou certo que vou usar ainda)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        img: '0 2px 6px rgba(var(--gray), 25%), 0 8px 24px rgba(var(--gray), 33%), 0 16px 32px rgba(var(--gray), 33%)',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
